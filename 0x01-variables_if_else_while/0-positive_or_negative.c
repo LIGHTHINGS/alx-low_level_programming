05ae935dd8983d18c0c3ef7d8fa53659ee87b2d0
Dare Omotayo <dareomotayo32@gmail.com>
2022-03-11 12:44:28 +0100

    Condtitional statments

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	if (n > 0)
+		printf("%d is positive \n", n);
+	else if (n < 0)
+		ptintf("%d is negative \n", n);
+	else
+		printf("%d is zero \n" , n);
+
+	return 0;
+}
