1cc5390e71c1f90a88d86705cf123579ad7cc205
Dare Omotayo <dareomotayo32@gmail.com>
2022-03-11 22:57:56 +0100

    Base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: this print base 10 numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d\n", n);
+		n++;
+	}
+	return (0);
+}
