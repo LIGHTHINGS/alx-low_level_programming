cc2fc0446ee7777336f1232bd166b8d9fb497f32
Dare Omotayo <dareomotayo32@gmail.com>
2022-03-12 22:34:31 +0100

    combo4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
-* Numbers must be separated by ,, followed by a space
-*The three digits must be different
+ * main - main block
+* Description: Numbers must be separated by ,, followed by a 
+* space. The three digits must be different
 *012, 120, 102, 021, 201, 210 are considered the same combination
 *of the three digits 0, 1 and 2
 *Print only the smallest combination of three digits
