8a3d8caa841578d1b16d5873525d7021a24834fc
Dare Omotayo <dareomotayo32@gmail.com>
2022-03-12 22:43:50 +0100

    combo4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,7 +2,7 @@
 
 /**
  *main - main block
-*Description: Numbers must be separated by ,, followed by a 
+*Description: Numbers must be separated by ,, followed by a
 *space. The three digits must be different
 *012, 120, 102, 021, 201, 210 are considered the same combination
 *of the three digits 0, 1 and 2
