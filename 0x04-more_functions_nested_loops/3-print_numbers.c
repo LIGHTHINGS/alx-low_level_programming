9e5e2e10e6619549253f5f37996e1c8f049b86e1
Dare Omotayo <dareomotayo32@gmail.com>
2022-03-18 06:06:47 +0100

    Second batch

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - to return numbers from
+ * 0 to 9
+ * Return: 0
+ */
+int print_numbers()
+{
+	int n = 48;
+	while ( n < 58 )
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
